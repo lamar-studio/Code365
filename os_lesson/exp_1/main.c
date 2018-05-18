8bbfbc0167e831ccd3ea42e4ba5b69dfcfea291a
lamar-studio <linzr_mail@163.com>
2018-05-18 21:50:03 +0800

    Init the os_lesson project.

---
+++ os_lesson/exp_1/main.c
@@ -0,0 +1,46 @@
+
+
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+
+
+
+int main()
+{
+    printf("hello world!\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
