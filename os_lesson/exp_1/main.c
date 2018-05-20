27d49f001682e79b2012f504e4421003b1599624
lamar-studio <linzr_mail@163.com>
2018-05-21 00:28:23 +0800

    Implement the exp_1 task -V1.0-

--- os_lesson/exp_1/main.c
+++ os_lesson/exp_1/main.c
@@ -1,49 +1,49 @@
 #include <stdio.h>  
 #include <mqueue.h>  
 #include <sys/stat.h>  
+#include <sys/prctl.h>
 #include <stdlib.h>  
 #include <unistd.h>  
 #include <time.h>  
-#include <string.h>  
+#include <string.h>
+#include <signal.h>
+
   
 #define MAXSIZE     10   //定义buf大小  
 #define BUFFER      8192 //定义Msg大小  
   
 struct MsgType{  
-    int len;  
-    char buf[MAXSIZE];  
     int index;  
     short op;  
 };
 
-struct LocalDataBase{
-    int index;
-    int count;
-};
 
-struct ShareDataBase{
-    int index;
-    int count;
-};
-  
-int main()   
+static int iExtFlag = 0;
+static int iExtFlag_c = 0;
+static int  msgCnt  = 0;
+static int localDatebase[MAXSIZE] = {0};
+static int shareDatabase[MAXSIZE] = {0};
+
+void parten_process();
+void child_process();
+
+
+int main(int argc, char ** argv)   
 {   
     /*消息队列*/  
     mqd_t msgq_id;  
     struct MsgType msg;
-
-    struct LocalDataBase ldb[10];
-    struct ShareDataBase sdb[10];
   
     unsigned int prio = 1;  
     unsigned int send_size = BUFFER; 
 
     unsigned int sender; 
-    unsigned int recv_size = BUFFER;  
+    unsigned int recv_size = BUFFER;
+
   
     struct mq_attr msgq_attr;  
     const char *file = "/posix";
-  
+    
     /*mq_open() for creating a new queue (using default attributes) */  
     /*mq_open() 创建一个新的 POSIX 消息队列或打开一个存在的队列*/  
     msgq_id = mq_open(file, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);  
@@ -60,27 +60,42 @@ int main()
         exit(1);  
     }  
   
-    printf("Queue \"%s\":\n\t- stores at most %ld messages \
+    printf("\nQueue \"%s\":\n\t- stores at most %ld messages \
             \n\t- large at most %ld bytes each\n\t- currently holds %ld messages\n",   
         file, msgq_attr.mq_maxmsg, msgq_attr.mq_msgsize, msgq_attr.mq_curmsgs);
      
     /*setting the attributes of the queue        --  mq_setattr() */  
     /*mq_setattr() 设置消息队列的属性，设置时使用由 newattr 指针指向的 mq_attr 结构的信息。*/  
     /*属性中只有标志 mq_flasgs 里的 O_NONBLOCK 标志可以更改，其它在 newattr 内的域都被忽略 */  
-    if(mq_setattr(msgq_id, &msgq_attr, NULL) == -1)  
+    if(mq_setattr(msgq_id, &msgq_attr, NULL) == -1)
     {  
         perror("mq_setattr");
         exit(1);  
     }     
 
-    int pid = fork();  
-    int i = 0; 
+    int pid = fork();
+    int child_pid = 0;
+    int i = 0;
     if(pid == 0) {  
     //子进程
-        while(i < 10) //运行一个consumenr，为 10 ，同时运行两个consumer进程，为 5    
-        {  
-            msg.len = -1;  
-            memset(msg.buf, 0, MAXSIZE);  
+        child_pid = getpid();
+        sigset( SIGTERM, child_process );
+        while(1) //运行一个consumenr，为 10 ，同时运行两个consumer进程，为 5    
+        { 
+            
+            if(iExtFlag_c == 1)
+            {
+                
+                printf("the ShareDatabase:%d\t\n", msgCnt);
+                for( i = 0; i < MAXSIZE; i++)
+                {
+                    printf("child  >> shareDatabase[%d] = %d\n", i, shareDatabase[i]);
+                }
+                printf("SIGTERM exit the child process.\n");
+
+                return 0;
+            }
+
             msg.index = 0;  
             msg.op    = 0;  
         
@@ -91,49 +106,75 @@ int main()
             /*如果 prio 不是 NULL，那么它指向的内存用于返回收到消息相关的优先级。*/  
             if (mq_receive(msgq_id, (char*)&msg, recv_size, &sender) == -1)   
             {  
-                perror("mq_receive");  
+                perror("mq_receive");
                 exit(1);  
-            }  
-            printf("child  >> msg.len = %d, msg.buf = %s, msg.index = %d, msg.op = %d\n", msg.len, msg.buf, msg.index, msg.op);
+            }
+            //printf("child  >> msg.index = %d, msg.op = %d\n", msg.index, msg.op);
 
-            if ( msg.op == 1 )
-                msg.index = (++msg.index) % 10;
+            //upadte the localDataBase
+            if ( msg.op == 0)
+            {
+                shareDatabase[msg.index] -=  1;
+            } else if( msg.op == 1)
+            {
+                shareDatabase[msg.index] +=  1;
+            }
             else
-                msg.index = (--msg.index) % 10;
-
-            printf("childP >> msg.len = %d, msg.buf = %s, msg.index = %d, msg.op = %d\n", msg.len, msg.buf, msg.index, msg.op);
-        
-            i++;  
-            sleep(2);   
-        }  
+            {
+                printf("the opration format err.");  
+            }
+            msgCnt++;
+            usleep(500000);
+        }
     }  
     else if(pid > 0) {
     //父进程
-        while(i < 10) 
+    sigset( SIGTERM, parten_process );
+    printf("parten id=%d \n", getpid());
+    while(1) 
+    {  
+        msg.index = rand() % 10;  
+        msg.op = rand() % 2;
+
+        //printf("parten >> msg.index = %d, msg.op = %d\n", msg.index, msg.op);  
+
+        /*sending the message      --  mq_send() */  
+        /*mq_send() 把 msg_ptr 指向的消息加入由 mqdes 引用的消息队列里。*/  
+        /*参数 msg_len 指定消息 msg_ptr 的长度：这个长度必须小于或等于队列 mq_msgsize 属性的值。零长度的消息是允许。*/  
+        if(mq_send(msgq_id, (char*)&msg, sizeof(struct MsgType), prio) == -1)  
         {  
-            msg.len = i;
-            memset(msg.buf, 0, MAXSIZE);
-            sprintf(msg.buf, "0x%x", i);  
-            msg.index = rand() % 10;  
-            msg.op = rand() % 2;  
-
-            printf("parten >> msg.len = %d, msg.buf = %s, msg.index = %d, msg.op = %d\n", msg.len, msg.buf, msg.index, msg.op);  
-
-            /*sending the message      --  mq_send() */  
-            /*mq_send() 把 msg_ptr 指向的消息加入由 mqdes 引用的消息队列里。*/  
-            /*参数 msg_len 指定消息 msg_ptr 的长度：这个长度必须小于或等于队列 mq_msgsize 属性的值。零长度的消息是允许。*/  
-            if(mq_send(msgq_id, (char*)&msg, sizeof(struct MsgType), prio) == -1)  
-            {  
-                perror("mq_send");  
-                exit(1);  
-            }  
-
-            i++;  
-            sleep(1);     
+            perror("mq_send");
+            exit(1);  
         }  
-        
-        sleep(30); //等待消费者进程退出          
-    }  
+
+        //upadte the localDataBase
+        if ( msg.op == 0)
+        {
+            localDatebase[msg.index] -=  1;
+        } else if( msg.op == 1)
+        {
+            localDatebase[msg.index] +=  1;
+        }
+        else
+        {
+            printf("the opration format err.");  
+        }
+        sleep(1);
+        if( iExtFlag == 1)
+        {
+            kill( 0, SIGTERM );
+            printf("the LocalDataBase:\t\n");
+            for( i = 0; i < MAXSIZE; i++)
+            {
+                printf("child  >> localDatebase[%d] = %d\n", i, localDatebase[i]);
+            }
+    
+            printf("SIGTERM exit the parten process.\n");
+            wait();
+            break;
+        }
+    }
+    }
     else {  
         fprintf(stderr, "fork error!\n");  
         mq_close(msgq_id);
@@ -141,7 +182,9 @@ int main()
         exit(5);  
     }
 
-    /*closing the queue        -- mq_close() */  
+    printf("\n === end the main function ===\n");
+
+    /*closing the queue        -- mq_close() */ 
     /*mq_close() 关闭消息队列描述符 mqdes。如果调用进程在消息队列 mqdes 绑定了通知请求，*/  
     /*那么这个请求被删除，此后其它进程就可以绑定通知请求到此消息队列。*/  
     if(mq_close(msgq_id) == -1)  
@@ -162,13 +205,19 @@ int main()
 }  
 
 
+void parten_process()
+{    
+    iExtFlag = 1;
 
+    return ;
+}
 
-
-
-
-
-
+void child_process()
+{
+    iExtFlag_c = 1;
+    
+    return ;
+}
 
 
 
