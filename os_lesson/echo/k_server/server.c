c7ba4f0783d751cb48a4ea2163fa0bced81885fc
linzr <linzaorong@ruijie.com.cn>
2018-05-30 00:02:09 +0800

    add the logic for changer

--- os_lesson/echo/k_server/server.c
+++ os_lesson/echo/k_server/server.c
@@ -84,6 +84,7 @@ static void nl_recv_msg(struct sk_buff *skb)
 	int              msg_size;
 	char             msg[BUF_SIZE] = {0};
 	int              res;
+    char             client[10]    = {0};
 
 	printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
 
@@ -97,10 +98,17 @@ static void nl_recv_msg(struct sk_buff *skb)
     strncpy(msg, (char *)nlmsg_data(nlh), msg_size);
 	printk(KERN_INFO "msg from user:%s len:%d\n", msg, strlen(msg));
 
-    procfs_buffer[PROCFS_MAX_SIZE-1] = 0;
-    printk(KERN_INFO "the proc config:%s", procfs_buffer);
+    //procfs_buffer[PROCFS_MAX_SIZE-1] = 0;
+    //printk(KERN_INFO "the proc config:%s", procfs_buffer);
 
-    upper_process(msg, strlen(msg));
+    if (strstr(procfs_buffer, UPPER) != NULL){
+        upper_process(msg, strlen(msg));
+    }
+    else if(strstr(procfs_buffer, LOWER) != NULL){
+        lower_process(msg, strlen(msg));
+    } else {
+        normal_process(msg, strlen(msg));
+    }
 
 	//发送数据(多播)
 	skb_out = nlmsg_new(msg_size, 0);
