e681e6959a286ed44c2e94266313e8f49de92f80
LaMar <linzr_mail@163.com>
2018-10-28 00:42:18 +0800

    algorithms: add the build info for hash

--- algorithms/c/hash/2_imple/README.md
+++ algorithms/c/hash/2_imple/README.md
@@ -1,7 +1,10 @@
-# Hash algorithms
+# Hash algorithms(String support)
 
 Overview files **hash.h** and **hash.c**
 * sdbm
 * djb2
 * xor8 (8 bit)
 * adler_32 (32 bit)
+
+- build:
+gcc -o test hash.c test_program.c
--- algorithms/c/hash/2_imple/test_program.c
+++ algorithms/c/hash/2_imple/test_program.c
@@ -9,9 +9,11 @@
 int main(void)
 {
     char s[] = "name";
+	char t[] = "linzr";
 
     /* actual tests */
     printf("sdbm: %s --> %lld\n", s, sdbm(s));
+	printf("sdbm: %s --> %lld\n", t, sdbm(t));
     printf("djb2: %s --> %lld\n", s, djb2(s));
     printf("xor8: %s --> %i\n", s, xor8(s)); /* 8 bit */
     printf("adler_32: %s --> %i\n", s, adler_32(s)); /* 32 bit */
